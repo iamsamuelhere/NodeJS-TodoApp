65b4a6f2283dd152286217dd2d96fc7f7fda3c88
iamsamuelhere <samuelcoder365@gmail.com>
2020-09-03 20:58:15 +0530

    added mongodb

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ const express=require('express')
 const bodyParser=require('body-parser')
 const app=express()
 app.use(bodyParser.urlencoded({extended:true}))
-
+app.use(express.static('public'))
+const mongoose =require('mongoose')
+mongoose.connect('mongodb+srv://samuser:9742828481@cluster0.4w4jg.mongodb.net/mydb?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+.then((result)=>console.log("connected to db"))
+.catch((err)=>console.log(err))
 app.set('view engine','ejs')
 tasks=[];
+const todoSchema=new mongoose.Schema({
+  topic:{
+    type:String,
+    required:true
+  },
+  content:{
+    type:String,
+    required:true
+  }
+})
+
+const Todo=mongoose.model('todo',todoSchema)
+
 
 app.get('/',function(req,res){
   var options={weekday:'long',year:'numeric',month:'long',day:'numeric'}
@@ -22,24 +39,65 @@ app.get('/',function(req,res){
     var greet="🌜 Good Night"
   }
   day=today.toLocaleDateString('en-US',options)
-   res.render('index',{day:day,tasks:tasks,greet:greet})
+  
+  Todo.find(function(err,stored){
+    if(err)
+    console.log(err)
+    else{
+      
+      res.render('index',{day:day,tasks:stored,greet:greet})
+    }
+  }).sort({_id:-1})
+
+
+   
 })
 app.post('/',function(req,res){
   
-  if(req.body.Add){
-    task=req.body.todo
-    tasks.push(task)
-  }
-  else if(req.body.Remove){
-      tasks.pop()
-  }
-
+  // if(req.body.Add){
+  //   task=req.body
+  //  console.log(task)
+  //   tasks.push(task)
+  // }
+  // else if(req.body.Remove){
+  //     tasks.pop()
+  // }
+  
+  const userinput=new Todo({
+    topic:req.body.todo,
+    content:req.body.textarea
+  })
+  userinput.save();
   
   
   res.redirect('/')
 
   
 })
+
+app.post('/remove',function(req,res){
+  Todo.findByIdAndRemove({_id:req.body.Remove},function(err){
+    console.log(err)
+  
+  })
+ res.redirect('/')
+});
+
+
+
+
+
+app.post('/checkdel',function(req,res){
+  
+Todo.findByIdAndRemove({_id:req.body.che},function(err){
+  console.log(err)
+
+})
+res.redirect('/')
+})
+
+
+
 app.use(function(req,res){
   res.status(404).sendFile(__dirname+'/notfound.html')
 })
